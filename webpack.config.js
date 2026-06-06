8d58d6a482e6326cc0d1aec618273a28bc3927dd
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-06-06 19:25:46 +0200

    add and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './assets/js/script.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: './',
+    port: 3000,
+    open: true,
+    hot: true,
+    client: {
+        logging: 'warn',
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+};
