c8ea628b008d080d875c2d06007c002810288adc
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-06-06 21:03:49 +0200

    move functions to JSSlider class and change them to class methods

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './assets/js/script.js',
+  devtool: 'eval-source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
